62223249411c1c9fcb65a27a3d6907fc8c3c2308
leopii98 <rippergames98@gmail.com>
2025-11-07 17:08:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Chiediamo al visitatore di scegliere tra "pari" o "dispari"
+let sceltaUtente = prompt("Scegli: pari o dispari?").toLowerCase();
+
+// Chiediamo all'utente di inserire un numero da 1 a 5
+let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
