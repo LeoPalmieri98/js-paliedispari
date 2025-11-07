29c280aa32217f19e387ad1f759d47e33de5b904
leopii98 <rippergames98@gmail.com>
2025-11-07 17:43:46 +0100

    add/somma+numero.random

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+
 // Chiediamo al visitatore di scegliere tra "pari" o "dispari"
 let sceltaUtente = prompt("Scegli: pari o dispari?").toLowerCase();
 
 // Chiediamo all'utente di inserire un numero da 1 a 5
 let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
+// Controlliamo che il numero sia valido
+if (isNaN(numeroUtente)) {
+    alert("Non è un numero!");
+} else if (numeroUtente < 1 || numeroUtente > 5) {
+    alert("Devi inserire un numero valido da 1 a 5!");
+}
+
+// Generiamo numero casuale del computer
+let numeroComputer = Math.floor(Math.random() * 5)
+console.log("Il computer ha scelto:", numeroComputer);
+
+// Sommiamo i due numeri
+let somma = numeroUtente + numeroComputer;
+console.log("La somma dei numeri è:", somma);
